ac21a24c2c61b84505eadae54349d2b9f664fc01
Jacob Bildy <jbildy@uwo.ca>
2019-03-11 12:45:25 -0700

    updated comments

--- biotype.py
+++ biotype.py
@@ -35,23 +35,11 @@ instructions.grid(row=3, column=1)
 editor  = Text(root, font='Menlo 18', bg=darkish, fg=whitish, width=100, height=30)
 editor.grid(row=4, column=1)
 
-tags = ["grey", "pink", "green", "cyan", "whiteblue", "orange",
-        "red", "lightgreen", "greygreen", "hRed", "default"]
-
-editor.tag_config("grey", foreground=greyOut)
-editor.tag_config("pink", foreground=pink)
-editor.tag_config("green", foreground=code_green)
-editor.tag_config("cyan", foreground=cyan)
-editor.tag_config("whiteblue", foreground=whiteblue)
-editor.tag_config("orange", foreground=orange)
-editor.tag_config("red", foreground=red)
-editor.tag_config("lightgreen", foreground=lightgreen)
-editor.tag_config("greygreen", foreground=greygreen)
-editor.tag_config("hRed", background=red)
-editor.tag_config("default", foreground=whitish, background=darkish)
-
 fileStr = open('KanR_Basic_Part_Construction.txt').read()
 editor.insert(INSERT, fileStr)
+
+tags = ["grey", "pink", "green", "cyan", "whiteblue", "orange",
+        "red", "lightgreen", "greygreen", "hRed", "default"]
 
 class Util :
     def isPlasmid(pos):
@@ -64,97 +52,110 @@ class Util :
         else:
             return "pink"
 
-    def textStyle(self):
+    def textStyle(self, txt):
 
+        txt.tag_config("grey", foreground=greyOut)
+        txt.tag_config("pink", foreground=pink)
+        txt.tag_config("green", foreground=code_green, underline=True)
+        txt.tag_config("cyan", foreground=cyan)
+        txt.tag_config("whiteblue", foreground=whiteblue)
+        txt.tag_config("orange", foreground=orange)
+        txt.tag_config("red", foreground=red)
+        txt.tag_config("lightgreen", foreground=lightgreen)
+        txt.tag_config("greygreen", foreground=greygreen)
+        txt.tag_config("hRed", background=red)
+        txt.tag_config("default", foreground=whitish, background=darkish)
+        
         for t in tags:
-            editor.tag_remove(t, '1.0', END)
+            txt.tag_remove(t, '1.0', END)
 
-        pos = editor.search(r"-|>", '1.0', stopindex=END, regexp=True)
-
-        while pos != '':
-            editor.tag_add("grey", pos)
-            pos = editor.search(r"-|>", pos.__add__("+ 1 chars"), stopindex=END, regexp=True)
-
-        pos = editor.search(r"Ligate|Digest|PCR", '1.0', stopindex=END, regexp=True)
+        pos = txt.search(r"-|>", '1.0', stopindex=END, regexp=True)
 
         while pos != '':
+            txt.tag_add("grey", pos)
+            pos = txt.search(r"-|>", pos.__add__("+ 1 chars"), stopindex=END, regexp=True)
 
-            while (editor.get(pos) != "") & (editor.get(pos) != " ") & (editor.get(pos) != "\n"):
-                editor.tag_add("cyan", pos)
+        pos = txt.search(r"Ligate|Digest|PCR", '1.0', stopindex=END, regexp=True)
+
+        while pos != '':
+
+            while (txt.get(pos) != "") & (txt.get(pos) != " ") & (txt.get(pos) != "\n"):
+                txt.tag_add("cyan", pos)
                 pos = pos.__add__("+ 1 chars")
 
             color = util.seqColor(pos)
 
-            while (editor.get(pos) != "") & (editor.get(pos) != "\n"):
+            while (txt.get(pos) != "") & (txt.get(pos) != "\n"):
 
-                while (editor.get(pos) == " ") | (editor.get(pos) == "(") | (editor.get(pos) == ")") \
-                        | (editor.get(pos) == ",") | (editor.get(pos) == "/"):
+                while (txt.get(pos) == " ") | (txt.get(pos) == "(") | (txt.get(pos) == ")") \
+                        | (txt.get(pos) == ",") | (txt.get(pos) == "/"):
                     pos = pos.__add__("+ 1 chars")
                     color = util.seqColor(pos)
 
-                if editor.get(pos) == "\n":
+                if txt.get(pos) == "\n":
                     break
 
-                if (editor.get(pos) == "a") & (editor.get(pos.__add__("+ 1 chars")) == "n") & \
-                        (editor.get(pos.__add__("+ 2 chars")) == "d") & (editor.get(pos.__add__("+ 3 chars")) == " "):
+                if (txt.get(pos) == "a") & (txt.get(pos.__add__("+ 1 chars")) == "n") & \
+                        (txt.get(pos.__add__("+ 2 chars")) == "d") & (txt.get(pos.__add__("+ 3 chars")) == " "):
                     pos = pos.__add__("+ 4 chars")
                     color = util.seqColor(pos)
-                if (editor.get(pos) == "o") & (editor.get(pos.__add__("+ 1 chars")) == "n") & \
-                        (editor.get(pos.__add__("+ 3 chars")) == " "):
+                if (txt.get(pos) == "o") & (txt.get(pos.__add__("+ 1 chars")) == "n") & \
+                        (txt.get(pos.__add__("+ 3 chars")) == " "):
 
                     pos = pos.__add__("+ 3 chars")
                     color = util.seqColor(pos)
 
                 else:
-                    editor.tag_add(color, pos)
+                    txt.tag_add(color, pos)
                     pos = pos.__add__("+ 1 chars")
 
-            pos = editor.search(r"Ligate|Digest|PCR", pos.__add__("+ 1 chars"), stopindex=END, regexp=True)
+            pos = txt.search(r"Ligate|Digest|PCR", pos.__add__("+ 1 chars"), stopindex=END, regexp=True)
 
-        pos = editor.search(r"EcoRI|SpeI|BsmI", '1.0', stopindex=END, regexp=True)
+        pos = txt.search(r"EcoRI|SpeI|BsmI", '1.0', stopindex=END, regexp=True)
 
         while pos != '':
 
-            while (editor.get(pos) != "") & (editor.get(pos) != " ") & (editor.get(pos) != "\n"):
-                editor.tag_add("green", pos)
+            while (txt.get(pos) != "") & (txt.get(pos) != " ") & (txt.get(pos) != "\n") \
+                    & (txt.get(pos) != "/") & (txt.get(pos) != ","):
+                txt.tag_add("green", pos)
                 pos = pos.__add__("+ 1 chars")
 
-            pos = editor.search(r"EcoRI|SpeI|BsmI", pos.__add__("+ 1 chars"), stopindex=END, regexp=True)
+            pos = txt.search(r"EcoRI|SpeI|BsmI", pos.__add__("+ 1 chars"), stopindex=END, regexp=True)
 
-        pos = editor.search(r"GAATTC|gaattc|ACTAGT|actagt", '1.0', stopindex=END, regexp=True)
+        pos = txt.search(r"GAATTC|gaattc|ACTAGT|actagt", '1.0', stopindex=END, regexp=True)
 
         while pos != '':
 
             for x in range(6):
-                editor.tag_add("hRed", pos)
+                txt.tag_add("hRed", pos)
                 pos = pos.__add__("+ 1 chars")
 
-            pos = editor.search(r"GAATTC|gaattc|ACTAGT|actagt", pos.__add__("+ 1 chars"), stopindex=END, regexp=True)
+            pos = txt.search(r"GAATTC|gaattc|ACTAGT|actagt", pos.__add__("+ 1 chars"), stopindex=END, regexp=True)
 
-        pos = editor.search(r"and|on|with", '1.0', stopindex=END, regexp=True)
+        pos = txt.search(r"and|on|with", '1.0', stopindex=END, regexp=True)
 
         while pos != '':
 
-            while (editor.get(pos) != "") & (editor.get(pos) != " ") & (editor.get(pos) != "\n"):
-                editor.tag_add("whiteblue", pos)
+            while (txt.get(pos) != "") & (txt.get(pos) != " ") & (txt.get(pos) != "\n"):
+                txt.tag_add("whiteblue", pos)
                 pos = pos.__add__("+ 1 chars")
 
-            pos = editor.search(r"and|on|with", pos.__add__("+ 1 chars"), stopindex=END, regexp=True)
+            pos = txt.search(r"and|on|with", pos.__add__("+ 1 chars"), stopindex=END, regexp=True)
 
-        pos = editor.search("bp", '1.0', stopindex=END)
+        pos = txt.search("bp", '1.0', stopindex=END)
 
         while pos != '':
 
             pos2 = pos
             pos = pos.__add__("+ 1 chars")
 
-            while (editor.get(pos) != "") & (editor.get(pos) != " ") & (editor.get(pos) != "("):
-                editor.tag_add("orange", pos)
+            while (txt.get(pos) != "") & (txt.get(pos) != " ") & (txt.get(pos) != "("):
+                txt.tag_add("orange", pos)
                 pos = pos.__add__("- 1 chars")
 
-            pos = editor.search(r"bp", pos2.__add__("+ 1 chars"), stopindex=END, regexp=True)
+            pos = txt.search(r"bp", pos2.__add__("+ 1 chars"), stopindex=END, regexp=True)
 
-        pos = editor.search(">", '1.0', stopindex=END)
+        pos = txt.search(">", '1.0', stopindex=END)
 
         while pos != '':
 
@@ -162,31 +163,34 @@ class Util :
 
             color = util.seqColor(pos)
 
-            while (editor.get(pos) != "") & (editor.get(pos) != "\n"):
+            while (txt.get(pos.__add__("+ 1 chars")) != txt.get(pos.__add__(" lineend"))):
 
-                if editor.get(pos) == " ":
-                    pos = pos.__add__("+ 1 chars")
+                if txt.get(pos) == " ":
                     color = "greygreen"
-                if (editor.get(pos) == "a") & (editor.get(pos.__add__("+ 1 chars")) == "n") & \
-                        (editor.get(pos.__add__("+ 2 chars")) == "d") & (editor.get(pos.__add__("+ 3 chars")) == " "):
+                    if not (txt.get(pos.__add__("+ 1 chars")) != txt.get(pos.__add__(" lineend"))) :
+                        pos = pos.__add__("+ 1 chars")
+
+                if (txt.get(pos) == "a") & (txt.get(pos.__add__("+ 1 chars")) == "n") & \
+                        (txt.get(pos.__add__("+ 2 chars")) == "d") & (txt.get(pos.__add__("+ 3 chars")) == " "):
                     pos = pos.__add__("+ 4 chars")
                     # color = util.seqColor(pos)
-                if (editor.get(pos) == "o") & (editor.get(pos.__add__("+ 1 chars")) == "n") & \
-                        (editor.get(pos.__add__("+ 3 chars")) == " "):
+                if (txt.get(pos) == "o") & (txt.get(pos.__add__("+ 1 chars")) == "n") & \
+                        (txt.get(pos.__add__("+ 3 chars")) == " "):
 
                     pos = pos.__add__("+ 3 chars")
                     # color = util.seqColor(pos)
 
                 else:
-                    editor.tag_add(color, pos)
+                    txt.tag_add(color, pos)
                     pos = pos.__add__("+ 1 chars")
 
-            pos = editor.search(">", pos.__add__("+ 1 chars"), stopindex=END)
+            pos = txt.search(">", pos.__add__("+ 1 chars"), stopindex=END)
 
 
 util = Util
 
-util.textStyle(util)
+util.textStyle(util, editor)
+util.textStyle(util, instructions)
 
 def running():
 
@@ -197,7 +201,8 @@ def running():
         i += 1
 
         if (i % 20 == 0) :
-            util.textStyle(util)
+            util.textStyle(util,  editor)
+            util.textStyle(util, instructions)
 
         root.update_idletasks()
         root.update()
