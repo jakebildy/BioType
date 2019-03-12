01ba81d24a5f61bf4f80f4eb020f0f9ec7efa3e7
Jacob Bildy <jbildy@uwo.ca>
2019-03-11 17:37:33 -0700

    Added modular enzymes

--- biotype.py
+++ biotype.py
@@ -24,32 +24,75 @@ root.configure(cursor="arrow")
 Label(root, text="BioType", font='Futura 30 bold', bg=darkish, fg=whitish).grid(row=0, column=1)
 Label(root, text="Construction File Visualizer", font='Futura 15', bg=darkish, fg=whitish).grid(row=1, column=1)
 
-sidebar  = Frame(root, bg=darkish, width=30, height=30)
+sidebar  = Frame(root, bg=darkish, width=60, height=30)
 sidebar.grid(row=3, column=0)
-sidebar  = Frame(root, bg=darkish, width=30, height=30)
+sidebar  = Frame(root, bg=darkish, width=60, height=30)
 sidebar.grid(row=3, column=2)
-
-instructions  = Text(root, font='Menlo 18', bg=darkish, fg=whitish, width=100, height=6,
-            highlightbackground=greyOut, highlightthickness=1, insertbackground=whitish,insertwidth=4,cursor="arrow")
-instructions.grid(row=3, column=1)
 
 editor  = Text(root, font='Menlo 18', bg=darkish, fg=whitish, width=100, height=30,
                highlightbackground=greyOut, highlightthickness=2, insertbackground=whitish,insertwidth=4,cursor="arrow")
 editor.grid(row=4, column=1)
-
 fileStr = open('KanR_Basic_Part_Construction.txt').read()
 editor.insert(INSERT, fileStr)
 
+
 tags = ["grey", "pink", "green", "cyan", "whiteblue", "orange",
-        "red", "lightgreen", "greygreen", "hRed", "default"]
+        "red", "lightgreen", "greygreen", "hRed", "default", "error"]
+
+enzymes =[]
+
+
+class Enzyme :
+
+    name = ""
+    site = "" #5'-3'
+    siteRC = "" #3'-5'
+    color = ""
+    cutBefore = 0
+
+    def addNew(self, n, s, src, c, before):
+        e  = Enzyme()
+        e.site = s
+        e.name = n
+        e.siteRC = src
+        e.color = c
+        e.cutBefore = before
+        enzymes.append(e)
+    def toHex(self, r,g,b):
+        return '#%02x%02x%02x' % (r, g, b)
+
+Enzyme.addNew(Enzyme, "EcoRI", "GAATTC", "GAATTC", Enzyme.toHex(Enzyme, 200, 240, 69),1)
+Enzyme.addNew(Enzyme, "SpeI", "ACTAGT", "TGATCA", Enzyme.toHex(Enzyme, 120, 120, 244), 1)
+
+
+class Sequence :
+
+    name = ""
+
+    def changeName (self, newName):
+        print()
+
+    def cutBefore(self, seq):
+        print()
+
+    def cutAfter(self, seq):
+        print()
+
+    def addPrimerF(self, primer):
+        print()
+
+    def addPrimerR(self, primer):
+        print()
+
+
 
 class Util :
-    def isPlasmid(pos):
-        if (editor.get(pos) == "p") &  (editor.get(pos.__add__("+ 1 chars")).isupper()):
+    def isPlasmid(pos, txt):
+        if (txt.get(pos) == "p") &  (txt.get(pos.__add__("+ 1 chars")).isupper()):
             return True
 
-    def seqColor(pos):
-        if util.isPlasmid(pos):
+    def seqColor(pos, txt):
+        if util.isPlasmid(pos, txt):
             return "lightgreen"
         else:
             return "pink"
@@ -75,6 +118,7 @@ class Util :
         txt.tag_config("greygreen", foreground=greygreen)
         txt.tag_config("hRed", background=red)
         txt.tag_config("default", foreground=whitish, background=darkish)
+        txt.tag_config("error", foreground=red, underline=True)
         
         for t in tags:
             txt.tag_remove(t, '1.0', END)
@@ -91,23 +135,23 @@ class Util :
                 txt.tag_add("cyan", pos)
                 pos = pos.__add__("+ 1 chars")
 
-            color = util.seqColor(pos)
+            color = util.seqColor(pos, txt)
 
             while (txt.get(pos.__add__("+ 1 chars")) != txt.get(pos.__add__(" lineend"))):
 
                 while (util.isCrap(pos, txt)):
                     pos = pos.__add__("+ 1 chars")
-                    color = util.seqColor(pos)
+                    color = util.seqColor(pos, txt)
 
                 if (txt.get(pos) == "a") & (txt.get(pos.__add__("+ 1 chars")) == "n") & \
                         (txt.get(pos.__add__("+ 2 chars")) == "d") & (txt.get(pos.__add__("+ 3 chars")) == " "):
                     pos = pos.__add__("+ 4 chars")
-                    color = util.seqColor(pos)
+                    color = util.seqColor(pos, txt)
                 if (txt.get(pos) == "o") & (txt.get(pos.__add__("+ 1 chars")) == "n") & \
                         (txt.get(pos.__add__("+ 3 chars")) == " "):
 
                     pos = pos.__add__("+ 3 chars")
-                    color = util.seqColor(pos)
+                    color = util.seqColor(pos, txt)
 
                 else:
                     txt.tag_add(color, pos)
@@ -119,24 +163,6 @@ class Util :
 
             pos = txt.search(r"Ligate|Digest|PCR", pos.__add__("+ 1 chars"), stopindex=END, regexp=True)
 
-        pos = txt.search(r"EcoRI|SpeI|BsmI", '1.0', stopindex=END, regexp=True)
-        while pos != '':
-
-            while (util.isCrap(pos, txt)):
-                txt.tag_add("green", pos)
-                pos = pos.__add__("+ 1 chars")
-
-            pos = txt.search(r"EcoRI|SpeI|BsmI", pos.__add__("+ 1 chars"), stopindex=END, regexp=True)
-
-
-        pos = txt.search(r"GAATTC|gaattc|ACTAGT|actagt", '1.0', stopindex=END, regexp=True)
-        while pos != '':
-
-            for x in range(6):
-                txt.tag_add("hRed", pos)
-                pos = pos.__add__("+ 1 chars")
-
-            pos = txt.search(r"GAATTC|gaattc|ACTAGT|actagt", pos.__add__("+ 1 chars"), stopindex=END, regexp=True)
 
         pos = txt.search(r"and|on|with", '1.0', stopindex=END, regexp=True)
         while pos != '':
@@ -155,6 +181,13 @@ class Util :
             txt.tag_add("orange", pos.__add__("+ 1 chars"))
             pos = txt.search(" L,", pos.__add__("+ 1 chars"), stopindex=END)
 
+        pos = txt.search(r"\d", '1.0', stopindex=END, regexp=True)
+        while (pos != ''):
+            while (txt.get(pos.__add__("- 1 chars")) == "+") | (txt.get(pos.__add__("- 1 chars")) == " ") | (("orange" in txt.tag_names(pos.__add__(" - 1 chars"))) & (txt.get(pos) in "1234567890")) :
+                txt.tag_add("orange", pos)
+                pos = pos.__add__("+ 1 chars")
+
+            pos = txt.search(r"\d", pos.__add__("+ 1 chars"), stopindex=END, regexp=True)
 
         pos = txt.search("bp", '1.0', stopindex=END)
         while pos != '':
@@ -181,7 +214,7 @@ class Util :
 
             pos = pos.__add__("+ 1 chars")
 
-            color = util.seqColor(pos)
+            color = util.seqColor(pos, txt)
 
             while (txt.get(pos.__add__("+ 1 chars")) != txt.get(pos.__add__(" lineend"))):
 
@@ -193,12 +226,12 @@ class Util :
                 if (txt.get(pos) == "a") & (txt.get(pos.__add__("+ 1 chars")) == "n") & \
                         (txt.get(pos.__add__("+ 2 chars")) == "d") & (txt.get(pos.__add__("+ 3 chars")) == " "):
                     pos = pos.__add__("+ 4 chars")
-                    # color = util.seqColor(pos)
+                    # color = util.seqColor(pos, txt)
                 if (txt.get(pos) == "o") & (txt.get(pos.__add__("+ 1 chars")) == "n") & \
                         (txt.get(pos.__add__("+ 3 chars")) == " "):
 
                     pos = pos.__add__("+ 3 chars")
-                    # color = util.seqColor(pos)
+                    # color = util.seqColor(pos, txt)
 
                 else:
                     txt.tag_add(color, pos)
@@ -209,11 +242,35 @@ class Util :
 
             pos = txt.search(">", pos.__add__("+ 1 chars"), stopindex=END)
 
+        for enzyme in enzymes :
+
+            txt.tag_config(enzyme.name, foreground=enzyme.color, underline=True)
+            txt.tag_config(enzyme.name+"_site", foreground="black", background=enzyme.color)
+
+            pos = txt.search(enzyme.name, '1.0', stopindex=END, regexp=True)
+
+            while pos != '':
+                while not (util.isCrap(pos, txt)):
+                    txt.tag_add(enzyme.name, pos)
+                    pos = pos.__add__("+ 1 chars")
+
+                pos = txt.search(enzyme.name, pos.__add__("+ 1 chars"), stopindex=END, regexp=True)
+
+            regex = r"(?i)("+enzyme.site +"|"+enzyme.siteRC+").*"
+
+            pos = txt.search(regex, '1.0', stopindex=END, regexp=True)
+            while pos != '':
+
+                for x in range(6):
+                    txt.tag_add(enzyme.name+"_site", pos)
+                    pos = pos.__add__("+ 1 chars")
+
+                pos = txt.search(regex, pos.__add__("+ 1 chars"), stopindex=END, regexp=True)
+
 
 util = Util
 
 util.textStyle(util, editor)
-util.textStyle(util, instructions)
 
 def running():
 
@@ -225,7 +282,6 @@ def running():
 
         if (i % 100 == 0) :
             util.textStyle(util,  editor)
-            util.textStyle(util, instructions)
 
         root.update_idletasks()
         root.update()
@@ -243,11 +299,15 @@ top = root.winfo_toplevel()
 menuBar = Menu(top)
 top['menu'] = menuBar
 subMenu = Menu(menuBar)
+enzymeMenu = Menu(menuBar)
 
 menuBar.add_cascade(label='Open Construction File', menu=subMenu)
+menuBar.add_cascade(label='Enzymes', menu=enzymeMenu)
 
-button = Button(root, text='▶', font='Futura 35', borderwidth=0, highlightbackground="black", bg=darkish, fg=code_green).grid(row=2,column=0)
-label = Label(root, text=' Run PCR', bg=darkish,fg=whitish, font="Futura 20").grid(row=2,column=1, sticky='w')
+label = Label(root, text='▶ Run PCR', bg=darkish,fg=whitish, font="Futura 20")
+
+label.grid(row=3,column=1, sticky='e')
+
 
 running()
 
